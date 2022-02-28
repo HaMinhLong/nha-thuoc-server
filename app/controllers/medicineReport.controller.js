c25b836cc30286f4df3c31baf85c1e14b0de6595
HaMinhLong <haminhlong3@gmail.com>
2022-02-28 11:02:02 +0700

    employee - supplier report

--- app/controllers/medicineReport.controller.js
+++ app/controllers/medicineReport.controller.js
@@ -15,14 +15,15 @@ const customerReport = async (req, res) => {
   const toDate = filters.toDate || moment().format();
 
   const report = await Sequelize.query(
-    `SELECT C.customerName, C.mobile, medicineName, MIM.price, MIM.amount
+    `SELECT DISTINCT RM.medicineId, C.customerName, C.mobile, medicineName, MIM.price, MIM.amount
     , MIM.discount, MIM.tax, MIM.price * MIM.amount AS totalRevenue, MIM.total
-    , MI.createdAt, MIM.total - (RM.price * MIM.amount) AS profit 
-    FROM customers AS C
-    JOIN medicineIssues AS MI ON MI.customerId = C.id
-    JOIN medicineIssueMedicines AS MIM ON MIM.medicineIssueId = MI.id
+    , MIM.total - (RM.price * MIM.amount) AS profit
+    , MIM.discountType, MIM.taxType, MI.debit
+    FROM medicineIssueMedicines AS MIM 
+    JOIN medicineIssues AS MI ON MI.id = MIM.medicineIssueId
     JOIN medicines AS M ON M.id = MIM.medicineId
     JOIN receiptMedicines AS RM ON RM.medicineId = M.id
+    JOIN customers AS C ON C.id = MI.customerId
     WHERE C.healthFacilityId LIKE :healthFacilityId AND C.customerName LIKE :customerName
     AND C.mobile LIKE :mobile AND MI.createdAt >= :fromDate AND MI.createdAt <= :toDate;
     `,
@@ -49,6 +50,99 @@ const customerReport = async (req, res) => {
   });
 };
 
+const employeeReport = async (req, res) => {
+  const { filter } = req.query;
+  const filters = filter ? JSON.parse(filter) : {};
+  const fullName = `%${filters.fullName ? filters?.fullName : ""}%`;
+  const mobile = `%${filters.mobile ? filters?.mobile : ""}%`;
+  const healthFacilityId = `%${
+    filters.healthFacilityId ? filters?.healthFacilityId : ""
+  }%`;
+  const fromDate = filters.fromDate || "2021-01-01T14:06:48.000Z";
+  const toDate = filters.toDate || moment().format();
+
+  const report = await Sequelize.query(
+    `SELECT U.fullName, U.mobile, medicineName, MIM.price, MIM.amount
+    , MIM.discount, MIM.tax, MIM.price * MIM.amount AS totalRevenue, MIM.total
+    , MIM.discountType, MIM.taxType, MI.debit
+    FROM medicineIssueMedicines AS MIM
+    JOIN medicineIssues AS MI ON MI.id = MIM.medicineIssueId
+    JOIN medicines AS M ON M.id = MIM.medicineId
+    JOIN users AS U ON U.id = MI.userId
+    WHERE MI.healthFacilityId LIKE :healthFacilityId AND U.fullName LIKE :fullName
+    AND U.mobile LIKE :mobile AND MI.createdAt >= :fromDate AND MI.createdAt <= :toDate;
+    `,
+    {
+      replacements: {
+        healthFacilityId: healthFacilityId,
+        fullName: fullName,
+        mobile: mobile,
+        fromDate: fromDate,
+        toDate: toDate,
+      },
+      type: QueryTypes.SELECT,
+    }
+  );
+
+  res.status(200).json({
+    results: {
+      list: report,
+      pagination: {},
+    },
+    success: true,
+    error: "",
+    message: "",
+  });
+};
+
+const supplierReport = async (req, res) => {
+  const { filter } = req.query;
+  const filters = filter ? JSON.parse(filter) : {};
+  const supplierName = `%${filters.supplierName ? filters?.supplierName : ""}%`;
+  const mobile = `%${filters.mobile ? filters?.mobile : ""}%`;
+  const healthFacilityId = `%${
+    filters.healthFacilityId ? filters?.healthFacilityId : ""
+  }%`;
+  const fromDate = filters.fromDate || "2021-01-01T14:06:48.000Z";
+  const toDate = filters.toDate || moment().format();
+
+  const report = await Sequelize.query(
+    `SELECT supplierName, mobile, medicineName
+    , RM.price, RM.amount, RM.discount, RM.discountType
+    , RM.tax, RM.taxType, RM.total, R.debit
+    , RM.price * RM.amount AS totalRevenue
+    FROM receiptMedicines AS RM
+    JOIN receipts AS R ON R.id = RM.receiptId
+    JOIN medicines AS M ON M.id = RM.medicineId
+    JOIN suppliers AS S ON S.id = R.supplierId
+    WHERE R.healthFacilityId LIKE :healthFacilityId AND S.supplierName LIKE :supplierName
+    AND S.mobile LIKE :mobile AND R.createdAt >= :fromDate AND R.createdAt <= :toDate;
+    `,
+    {
+      replacements: {
+        healthFacilityId: healthFacilityId,
+        supplierName: supplierName,
+        mobile: mobile,
+        fromDate: fromDate,
+        toDate: toDate,
+      },
+      type: QueryTypes.SELECT,
+    }
+  );
+
+  res.status(200).json({
+    results: {
+      list: report,
+      pagination: {},
+    },
+    success: true,
+    error: "",
+    message: "",
+  });
+};
+
 module.exports = {
   customerReport,
+  employeeReport,
+  supplierReport,
 };
--- app/routes/medicineReport.routes.js
+++ app/routes/medicineReport.routes.js
@@ -2,4 +2,6 @@ const controller = require("../controllers/medicineReport.controller");
 
 module.exports = function (app) {
   app.get("/customerReport", controller.customerReport);
+  app.get("/employeeReport", controller.employeeReport);
+  app.get("/supplierReport", controller.supplierReport);
 };
